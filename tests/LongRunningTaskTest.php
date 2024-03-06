c88694957ee0dd7dea2376f65b3e0678640a9ba0
Freek Van der Herten <freek@spatie.be>
2024-03-06 17:06:16 +0100

    wip

--- tests/LongRunningTaskTest.php
+++ tests/LongRunningTaskTest.php
@@ -1,12 +1,14 @@
 <?php
 
 use Clickonmedia\Monitor\Enums\LogItemStatus;
+use Clickonmedia\Monitor\Jobs\RunLongRunningTaskJob;
 use Clickonmedia\Monitor\Models\LongRunningTaskLogItem;
 use Clickonmedia\Monitor\Tests\TestSupport\LongRunningTasks\LongRunningTestTask;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Queue;
 
 it('can create create a pending task', function () {
-    Bus::fake();
+    Queue::fake();
 
     LongRunningTestTask::make()->start();
 
@@ -18,4 +20,6 @@ it('can create create a pending task', function () {
         ->attempt->toBe(1)
         ->check_frequency_in_seconds->toBe(10)
         ->meta->toBe([]);
+
+    Queue::assertPushed(RunLongRunningTaskJob::class);
 });
