2a4779dfd532617142ccebbfb827c8b7cfca3599
Freek Van der Herten <freek@spatie.be>
2024-03-11 08:26:06 +0100

    add comments to config file

--- config/long-running-tasks-monitor.php
+++ config/long-running-tasks-monitor.php
@@ -1,11 +1,26 @@
 <?php
 
 return [
+    /*
+     * When a task is not completed in this amount of time,
+     * it will not run again, and marked as `didNotComplete`.
+     */
     'keep_checking_for_in_seconds' => 60 * 5,
 
+    /*
+     * If a task determines that it should be continued, it will
+     * be called again after this amount of time
+     */
     'default_check_frequency_in_seconds' => 10,
 
+    /*
+     * The model that will be used by default to track
+     * the status of all tasks.
+     */
     'log_model' => Clickonmedia\Monitor\Models\LongRunningTaskLogItem::class,
 
+    /*
+     * The job responsible for calling tasks.
+     */
     'task_job' => Clickonmedia\Monitor\Jobs\RunLongRunningTaskJob::class,
 ];
