e6c4d74c20252eb58376f002196040dfad06de86
freekmurze <freekmurze@users.noreply.github.com>
2024-03-12 12:48:37 +0000

    Fix styling

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -4,8 +4,9 @@ use Clickonmedia\Monitor\Exceptions\InvalidModel;
 use Clickonmedia\Monitor\Models\LongRunningTaskLogItem;
 use Clickonmedia\Monitor\Support\Config;
 
-it('can handle a valid model', function() {
-    $customModel = new class extends LongRunningTaskLogItem {
+it('can handle a valid model', function () {
+    $customModel = new class extends LongRunningTaskLogItem
+    {
         protected $table = 'long_running_task_log_items';
     };
 
@@ -16,7 +17,7 @@ it('can handle a valid model', function() {
     expect($modelClass)->toBe($customModel::class);
 });
 
-it('will throw an exception for an invalid model', function() {
+it('will throw an exception for an invalid model', function () {
     config()->set('long-running-tasks-monitor.log_model', Config::class);
 
     Config::getLongRunningTaskLogItemModelClass();
