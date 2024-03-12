f74cf1f9cfd6550f4720a05d3a94284e7453d09f
Freek Van der Herten <freek@spatie.be>
2024-03-12 13:53:23 +0100

    add test

--- database/factories/LongRunningTaskLogItemFactory.php
+++ database/factories/LongRunningTaskLogItemFactory.php
@@ -14,6 +14,7 @@ class LongRunningTaskLogItemFactory extends Factory
     {
         return [
             'type' => $this->faker->word,
+            'queue' => 'default',
             'status' => LogItemStatus::Pending,
             'check_frequency_in_seconds' => 10,
             'meta' => [],
--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,10 +1,12 @@
 <?php
 
 use Clickonmedia\Monitor\Exceptions\InvalidModel;
+use Clickonmedia\Monitor\Jobs\LongRunningTaskJob;
+use Clickonmedia\Monitor\Jobs\RunLongRunningTaskJob;
 use Clickonmedia\Monitor\Models\LongRunningTaskLogItem;
 use Clickonmedia\Monitor\Support\Config;
 
-it('can handle a valid model', function() {
+it('can handle a valid custom model', function () {
     $customModel = new class extends LongRunningTaskLogItem {
         protected $table = 'long_running_task_log_items';
     };
@@ -16,8 +18,24 @@ it('can handle a valid model', function() {
     expect($modelClass)->toBe($customModel::class);
 });
 
-it('will throw an exception for an invalid model', function() {
+it('will throw an exception for an invalid model', function () {
     config()->set('long-running-tasks-monitor.log_model', Config::class);
 
     Config::getLongRunningTaskLogItemModelClass();
 })->throws(InvalidModel::class);
+
+it('can handle a custom job class', function () {
+    $logItem = LongRunningTaskLogItem::factory()->create();
+
+    $customJob = new class($logItem) extends RunLongRunningTaskJob {
+
+    };
+
+    config()->set('long-running-tasks-monitor.task_job', $customJob::class);
+
+    $jobClass = Config::getTaskJobClass();
+
+    expect($jobClass)->toBe($customJob::class);
+
+
+});
