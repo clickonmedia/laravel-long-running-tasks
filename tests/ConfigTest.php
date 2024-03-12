b053dee33bebcac72b4ce5ff3ef9e1ba14fa953d
Freek Van der Herten <freek@spatie.be>
2024-03-12 20:26:09 +0100

    wip

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,12 +1,13 @@
 <?php
 
+use Clickonmedia\Monitor\Enums\TaskResult;
 use Clickonmedia\Monitor\Exceptions\InvalidModel;
-use Clickonmedia\Monitor\Jobs\LongRunningTaskJob;
 use Clickonmedia\Monitor\Jobs\RunLongRunningTaskJob;
 use Clickonmedia\Monitor\Models\LongRunningTaskLogItem;
 use Clickonmedia\Monitor\Support\Config;
 use Clickonmedia\Monitor\Tests\TestSupport\LongRunningTasks\LongRunningTestTask;
 use Illuminate\Support\Facades\Queue;
+use Clickonmedia\Monitor\LongRunningTask;
 
 it('can handle a valid custom model', function () {
     $customModel = new class extends LongRunningTaskLogItem {
@@ -25,6 +26,31 @@ it('will throw an exception for an invalid model', function () {
 
     Config::getLongRunningTaskLogItemModelClass();
 })->throws(InvalidModel::class);
+
+it('can use a custom model', function() {
+    $customModel = new class extends LongRunningTaskLogItem {
+        protected $table = 'long_running_task_log_items';
+    };
+
+    config()->set('long-running-tasks-monitor.log_model', $customModel::class);
+
+    $task = new class($customModel) extends LongRunningTask
+    {
+        public function __construct(protected string $customModel)
+        {
+
+        }
+
+        public function check(LongRunningTaskLogItem $logItem): TaskResult
+        {
+            if (! $logItem instanceof $this->customModel) {
+                throw new Exception('Not the right class');
+            }
+        }
+    };
+
+    $task->start();
+})->todo();
 
 it('can handle a custom job class', function () {
     $logItem = LongRunningTaskLogItem::factory()->create();
